a4900508c197c78cf29879e5013c2a43b684cd41
samabraha <samson.abraha@gmail.com>
2021-12-22 15:42:36 +0100

    Created Android HelloWorld app.

--- SQLiteTest/app/src/main/java/com/example/sqlitetest/FirstFragment.kt
+++ SQLiteTest/app/src/main/java/com/example/sqlitetest/FirstFragment.kt
@@ -11,7 +11,7 @@ import com.example.sqlitetest.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment() { 
 
     private var _binding: FragmentFirstBinding? = null
 
